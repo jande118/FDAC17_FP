7e053d3df96fb7f9871212bbf75e1a639ff51905
Jonathan Anderson <jande118@vols.utk.edu>
2017-12-01 06:17:25 -0500

    fixed small issues with process.py

--- jande118/scripts/process.py
+++ jande118/scripts/process.py
@@ -2,12 +2,15 @@ import sys
 import subprocess
 
 if len(sys.argv) < 2:
-    print('usage: python3 ./process.py [dependency file path] [dependency #1] [dependency #2] ...')
+    print('usage: python3 process.py [dependency #1] [dependency #2] ...')
     sys.exit()
-DATA = str(sys.argv[1])
+
+DATA = '../data/extracted/dependencies.csv'
 for dep in sys.argv:
-    if dep == sys.argv[0] or dep == sys.argv[1]: # ignore process.py and dependency.csv
+    if dep == sys.argv[0]: # ignore process.py and dependency.csv
         continue
-    OUT = dep + '.data' # Build output file
-    cmd = 'egrep \"^([^,]+,){1}' + dep + ',\" '+ DATA + ' > ' + OUT # Build command
-    print(subprocess.check_output([cmd], shell=True))
+    OUT = '../data/new_data/' + dep + '.data' # Build output file
+    egrep = 'egrep \"^([^,]+,){1}' + dep + ',\" ' + DATA + ' > ' + OUT # Build the egrep command
+    cmd = [egrep]
+    #print(DATA); print(egrep); print(cmd) # DEBUGGING
+    print(subprocess.check_output(cmd, shell=True))
